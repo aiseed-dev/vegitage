2b7345b1eaf32497225c7e8fbe26f7091cc9f79f
Yasuhiro Niji <niji@yniji.net>
2025-12-25 14:49:40 +0900

    Flutter プロジェクトがサブディレクトリにあるための修正

--- frontend/vegitage/ios/ci_scripts/ci_post_clone.sh
+++ frontend/vegitage/ios/ci_scripts/ci_post_clone.sh
@@ -6,6 +6,9 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
+# Flutterプロジェクトのサブディレクトリに移動
+cd frontend/vegitage
+
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
